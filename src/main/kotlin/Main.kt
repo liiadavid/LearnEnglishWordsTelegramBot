391af996ba88d105f51bb872a4a6040f4c0fd111
liiadavid <liiadavid@yandex.ru>
2024-04-03 19:45:26 +0300

    LEWTB-stage-2 add dictionary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
-package org.example
+import java.io.File
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int? = 0,
+)
 
 fun main() {
-    println("Hello World!")
+    val wordsFile: File = File("words.txt")
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    wordsFile.createNewFile()
+
+    wordsFile.writeText("hello|привет|1\n")
+    wordsFile.appendText("dog|собака|0\n")
+    wordsFile.appendText("cat|кошка|0\n")
+
+    wordsFile.forEachLine { line ->
+        val word = line.split("|")
+        dictionary.add(Word(word[0], word[1], word[2].toIntOrNull() ?: 0))
+    }
+
+    dictionary.forEach {
+        println(it)
+    }
 }
