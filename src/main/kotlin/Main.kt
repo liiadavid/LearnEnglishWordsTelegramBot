bcc6d63de7738de2c78bd2bac8daf7639b8e23cb
liiadavid <liiadavid@yandex.ru>
2024-04-03 17:22:29 +0300

    LEWTB-stage-1 create words file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
-package org.example
+import java.io.File
 
 fun main() {
-    println("Hello World!")
+    val wordsFile: File = File("words.txt")
+
+    wordsFile.createNewFile()
+
+    wordsFile.writeText("hello привет\n")
+    wordsFile.appendText("dog собака\n")
+    wordsFile.appendText("cat кошка\n")
+
+    wordsFile.forEachLine {
+        println(it)
+    }
 }
