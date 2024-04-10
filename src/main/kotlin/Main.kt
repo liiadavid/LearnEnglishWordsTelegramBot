20ffcad6c0348a1903fe6f4320387daba0f49ed8
liiadavid <liiadavid@yandex.ru>
2024-04-10 15:22:28 +0300

    LEWTB-stage-5 add break

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -72,6 +72,7 @@ fun main() {
                             else -> println("Ответ неверный.")
                         }
                     }
+                    break
                 }
             }
 
