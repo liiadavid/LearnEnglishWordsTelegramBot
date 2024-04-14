c9c3ef5a9b208286b26389b9c3a5873235253c4a
liiadavid <liiadavid@yandex.ru>
2024-04-14 21:38:31 +0300

    LEWTB-stage-8 add TelegramBotService.kt

---
+++ src/main/kotlin/TelegramBotService.kt
@@ -0,0 +1,26 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class TelegramBotService {
+    fun getUpdates(botToken: String, updateId: Int): String {
+        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        return response.body()
+    }
+
+    fun sendMessage(botToken: String, chatId: String?, message: String?): String {
+        var text = ""
+        if (message.equals("Hello")) text = "Hello"
+        val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$text"
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
+        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        return response.body()
+    }
+}
